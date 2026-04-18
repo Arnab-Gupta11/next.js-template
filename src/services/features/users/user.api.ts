20a372e63b89e4baa6e6a4def5519cc5dd0ad184
Arnab Gupta <arnabgupta1111@gmail.com>
2026-04-19 04:21:32 +0600

    fixed

--- src/services/features/users/user.api.ts
+++ src/services/features/users/user.api.ts
@@ -5,52 +5,57 @@ import { queryKeys } from '@/services/config/query-keys';
 import { userService } from './user.services';
 
 const UserApi = {
+  //=====================
   // Queries
-  useAllUsers: (params?: any) =>
-    useAppQuery(queryKeys.users.list(params), () => userService.getAll(params)),
+  //=====================
+  queries: {
+    useAllUsersQuery: (params?: any) =>
+      useAppQuery(queryKeys.users.list(params), () => userService.getAll(params)),
 
-  useUserSummary: () => useAppQuery(queryKeys.users.summary(), () => userService.getSummary()),
+    useUserSummaryQuery: () =>
+      useAppQuery(queryKeys.users.summary(), () => userService.getSummary()),
 
-  useMe: () => useAppQuery(queryKeys.users.me(), () => userService.getMe()),
+    useMeQuery: () => useAppQuery(queryKeys.users.me(), () => userService.getMe()),
+  },
 
+  //=====================
   // Mutations
-  useCreateUser: () =>
-    useAppMutation((data) => userService.create(data), {
-      invalidateKeys: [queryKeys.users.lists()],
-    }),
+  //=====================
+  mutations: {
+    useCreateUserMutation: () =>
+      useAppMutation((data) => userService.create(data), {
+        invalidateKeys: [queryKeys.users.lists()],
+      }),
 
-  useUpdateUser: (userId: string) =>
-    useAppMutation(({ id, data }: { id: string; data: any }) => userService.update(id, data), {
-      invalidateKeys: [queryKeys.users.lists(), queryKeys.users.detail(userId)],
-    }),
+    useUpdateUserMutation: (userId: string) =>
+      useAppMutation(({ id, data }: { id: string; data: any }) => userService.update(id, data), {
+        invalidateKeys: [queryKeys.users.lists(), queryKeys.users.detail(userId)],
+      }),
 
-  useUpdateAvatar: () =>
-    useAppMutation((data) => userService.updateAvatar(data), {
-      invalidateKeys: [queryKeys.users.me()],
-    }),
+    useUpdateAvatarMutation: () =>
+      useAppMutation((data) => userService.updateAvatar(data), {
+        invalidateKeys: [queryKeys.users.me()],
+      }),
 
-  useDeleteUser: () =>
-    useAppMutation((id: string) => userService.delete(id), {
-      invalidateKeys: [queryKeys.users.lists()],
-    }),
+    useDeleteUserMutation: () =>
+      useAppMutation((id: string) => userService.delete(id), {
+        invalidateKeys: [queryKeys.users.lists()],
+      }),
 
-  useRestoreUser: () =>
-    useAppMutation((id: string) => userService.restore(id), {
-      invalidateKeys: [queryKeys.users.lists()],
-    }),
-
-  useGetConversationId: () =>
-    useAppMutation((recipientId: string) => userService.getConversationId(recipientId)),
+    useRestoreUserMutation: () =>
+      useAppMutation((id: string) => userService.restore(id), {
+        invalidateKeys: [queryKeys.users.lists()],
+      }),
+  },
 };
 
 export const {
-  useAllUsers,
-  useUserSummary,
-  useMe,
-  useCreateUser,
-  useUpdateUser,
-  useUpdateAvatar,
-  useDeleteUser,
-  useRestoreUser,
-  useGetConversationId,
-} = UserApi;
+  useAllUsersQuery,
+  useUserSummaryQuery,
+  useMeQuery,
+  useCreateUserMutation,
+  useUpdateUserMutation,
+  useUpdateAvatarMutation,
+  useDeleteUserMutation,
+  useRestoreUserMutation,
+} = { ...UserApi.queries, ...UserApi.mutations };
